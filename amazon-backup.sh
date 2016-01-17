6398007f8a5405a2570d95cd37a5510da6e3cc97
Ammon Smith <ammon.i.smith@gmail.com>
2016-01-16 19:25:28 -0800

    Added encfs mounting.

--- amazon-backup.sh
+++ amazon-backup.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/sh
+set -e
 
-rclone sync /media/archive/Backup/Titus              Amazon\ Backup:/Live/Titus
-rclone sync /media/archive/Backup/Archive\ Disk      Amazon\ Backup:/Live/Archive
-rclone sync /media/archive/Backup/Nexus One.crypt    Amazon\ Backup:/Live/NexusOne
-rclone sync /media/archive/Backup/Nexus Five.crypt   Amazon\ Backup:/Live/NexusFive
-rclone sync /media/archive/Backup/Novus USB.crypt    Amazon\ Backup:/Live/NovusUSB
-rclone sync /media/archive/Backup/Tombo\ Notes.crypt Amazon\ Backup:/Live/TomboyNotes
+encfs /media/archive/Backup/Nexus\ One.crypt          /media/archive/Backup/Nexus\ One
+encfs /media/archive/Backup/Nexus\ Five.crypt         /media/archive/Backup/Nexus\ Five
+encfs /media/archive/Backup/Novus\ USB.crypt          /media/archive/Backup/Novus\ USB
+encfs /media/archive/Backup/Tomboy\ Notes.crypt       /media/archive/Backup/Tomboy\ Notes
+
+rclone sync /media/archive/Backup/Titus               Amazon\ Backup:/Live/Titus
+rclone sync /media/archive/Backup/Archive\ Disk       Amazon\ Backup:/Live/Archive
+rclone sync /media/archive/Backup/Nexus One.crypt     Amazon\ Backup:/Live/NexusOne
+rclone sync /media/archive/Backup/Nexus Five.crypt    Amazon\ Backup:/Live/NexusFive
+rclone sync /media/archive/Backup/Novus USB.crypt     Amazon\ Backup:/Live/NovusUSB
+rclone sync /media/archive/Backup/Tomboy\ Notes.crypt Amazon\ Backup:/Live/TomboyNotes
 
