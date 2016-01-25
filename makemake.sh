3aa56cbfb2e41d108b97a22e257c5f7c63b0c9b7
Ammon Smith <ammon.i.smith@gmail.com>
2016-01-24 23:52:05 -0800

    Renamed miniproject to makemake and centered lock.

---
+++ makemake.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+### FUNCTIONS ###
+usage_and_exit() {
+	printf >&2 'Usage: %s directory...\n' "$(basename $0)"
+	exit 1
+}
+
+write_makefile() {
+		cat << EOF > Makefile
+# Note for OBJECTS: list all *.o files necessary for compilation in the
+# given variable. They must be listed like this:
+# OBJECTS=abc.o \\
+#		def.o   \\
+#		main.o
+# You *must* use tabs in front of each object name, you must escape each line
+# with a backslash, like shown.
+
+.PHONY: all debug force distclean clean
+
+CC=g++
+FLAGS=-Wall -Werror
+OBJECTS=main.o
+EXE=main
+DEBUGGER=gdb
+
+all: \$(EXE)
+
+%.o: %.cpp
+	\$(CC) \$(FLAGS) \$(EXTRA_FLAGS) -c -o \$@ \$<
+
+\$(EXE): \$(OBJECTS)
+	\$(CC) \$(FLAGS) \$(EXTRA_FLAGS) -o \$(EXE) \$(OBJECTS)
+
+debug: clean
+	make \$(EXE) EXTRA_FLAGS=-g
+	\$(DEBUGGER) \$(EXE)
+
+force: clean \$(EXE)
+
+distclean:
+	rm -f *.o *~ \$(EXE) core core.*
+
+clean:
+	rm -f *.o \$(EXE)
+
+EOF
+}
+
+new_project() {
+	set -e
+	echo "[NEW] $1"
+	mkdir -p -- "$1"
+	cd -- "$1"
+	write_makefile
+	cd ..
+}
+
+update_project() {
+	if [[ ! -d $1 ]]; then
+		new_project
+		return $?
+	fi
+
+	set -e
+	echo "[UPDATE] $dir"
+	cd -- "$1"
+	write_makefile
+	cd ..
+}
+
+main() {
+	for dir in $@; do
+		if [[ -e $dir ]]; then
+			if [[ -d $dir ]]; then
+				update_project "$dir"
+			else
+				printf >&2 'Location exists and is not a directory: "%s".\n' "$dir"
+			fi
+		else
+			new_project "$dir"
+		fi
+	done
+}
+
+### MAIN ###
+[ $# -eq 0 ] && usage_and_exit
+cd "$(dirname $0)"
+[[ $1 == --help ]] && usage_and_exit
+main $@
+
