f7619ebb3dc9be0ca2efd8645b4be43d3ef64f2f
Ammon Smith <ammon.i.smith@gmail.com>
2016-02-28 19:29:51 -0800

    Added skip media script.

---
+++ wm/media/tired.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+case "$(/usr/local/scripts/wm/media/detect-audio-player.sh)" in
+    mocp) mocp --next ;;
+    pianobar) printf 't' > ~/.config/pianobar/ctl ;;
+    vlc) dbus-send --type=method_call --dest=org.mpris.MediaPlayer2.vlc /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next ;;
+    *) false ;;
+esac
+
+
