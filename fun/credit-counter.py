c0ee804bb55519af04c350b6e3adba8fe6a6bc09
Ammon Smith <ammon.i.smith@gmail.com>
2016-03-21 22:00:17 -0700

    Added credit counter.

---
+++ fun/credit-counter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""
+Automatically counts credits in CYOA-type games by looking
+for instances of +(number) and -(number) and adding it all up.
+"""
+
+from __future__ import with_statement
+import re
+import os
+import sys
+
+CREDIT_REGEX = re.compile(r"([+-][0-9]+)")
+
+
+def count_credits(text):
+    transactions = CREDIT_REGEX.findall(text)
+    print(transactions)
+    credits = 0
+
+    for transaction in transactions:
+        credits += int(transaction)
+
+    return credits
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage:\n%s path-to-file..." % os.path.basename(sys.argv[0]))
+        exit(1)
+
+    for fn in sys.argv[1:]:
+        with open(fn, 'r') as fh:
+            credits = count_credits(fh.read())
+            print("%s: %d credits" % (fn, credits))
+
