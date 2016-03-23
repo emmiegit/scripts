c39771980744313110dc06fc1e49e71f3ed4a0f1
Ammon Smith <ammon.i.smith@gmail.com>
2016-03-23 15:43:58 -0700

    Renamed scripts.

--- mouse-sensitivity.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xinput --set-prop 8 "Device Accel Velocity Scaling" 1
-xinput --set-prop 8 "Device Accel Profile" -1
-xinput --set-prop 8 "Device Accel Constant Deceleration" .6
-#xset m 1 0
-
