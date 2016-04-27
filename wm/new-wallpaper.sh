e126631c74216bfb01597d5b0848a29c10bb6a12
Ammon Smith <ammon.i.smith@gmail.com>
2016-04-27 11:27:04 -0700

    Fixed invalid variable in newwp

--- wm/new-wallpaper.sh
+++ wm/new-wallpaper.sh
@@ -5,8 +5,8 @@ SPREAD=bg-fill              # Strategy for displaying wallpapers. See "man feh".
 DIR=~/Pictures/Wallpapers   # Wallpaper directory. Section refers to the subdirectory.
 SECTION="$(cat /usr/local/scripts/dat/wallpaper_section)"
 
-[[ -n $1 ]] && SECTION="$1"
-
+[[ $# -gt 0 ]] && SECTION="$1"
 [[ -n $DISPLAY ]] && export DISPLAY=:0
+
 feh --recursive --randomize --"$SPREAD" "$DIR/$SECTION"
 
