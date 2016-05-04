6dcc88f72787cb5ede0d0d3e8475d7a94a8e017a
Ammon Smith <ammon.i.smith@gmail.com>
2016-05-04 00:19:24 -0700

    Update pack

--- arch/pack.sh
+++ arch/pack.sh
@@ -4,19 +4,19 @@ set -eu
 pack() {
     dest="$HOME/Documents/Relic/$1.zip"
     [[ -f "$dest" ]] && mv -f "$dest" "$dest~"
-    cd $2
+    cd "$2"
     7z a -tzip -mx=2 "$dest" *
     printf 'Archive created at %s composed of files in %s.\n' "$dest" "$2"
     cd - > /dev/null
 }
 
 main() {
-    pack wps        ~/Pictures/Wallpapers         &&
-    pack co.commit  ~/Pictures/Comics/commitstrip &&
-    pack co.dilbert ~/Pictures/Comics/dilbert     &&
-    pack co.smbc    ~/Pictures/Comics/smbc        &&
-    pack co.xkcd    ~/Pictures/Comics/xkcd        &&
-    pack mus        ~/Music                       &&
+    pack co.commit  ~/Pictures/Comics/commitstrip
+    pack co.dilbert ~/Pictures/Comics/dilbert
+    pack co.smbc    ~/Pictures/Comics/smbc
+    pack co.xkcd    ~/Pictures/Comics/xkcd
+    pack music      ~/Music
+    pack osu        /media/media/Games/osu\!/Songs
     read -p 'Finished. '
 }
 
