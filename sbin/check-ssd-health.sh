379bdb9acee66818188145c7761fb2202eb412ec
Ammon Smith <ammon.i.smith@gmail.com>
2016-05-27 11:43:28 -0700

    Hashing smbc comics and improve check-ssd-health.

--- sbin/check-ssd-health.sh
+++ sbin/check-ssd-health.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -eu
+
 # SSD health checker for the Samsung 850 EVO. Be sure to research your particular
 # model to look for the correct attribute name, and obviously the right device too.
 DEVICE='/dev/sdc'
@@ -7,5 +9,19 @@ get_health() {
     sudo smartctl -a "$DEVICE" | grep 'Wear_Leveling_Count' | awk '{print $4}'
 }
 
-echo "Life remaining for $DEVICE: $(get_health | perl -pe 'chomp')%"
+printf 'Check health of device "%s"? [Y/n] ' "$DEVICE"
+
+read -r response
+case "$response" in
+    n*)
+        printf 'Not checking device.\n'
+        exit 1
+        ;;
+    N*)
+        printf 'Not checking device.\n'
+        exit 1
+        ;;
+esac
+
+printf 'Life remaining for "%s": %s%%\n' "$DEVICE" "$(get_health | perl -pe 'chomp')"
 
