3d2f4c91dd9966a606b39659415faddbb7851c9d
Ammon Smith <ammon.i.smith@gmail.com>
2016-06-18 16:51:56 -0700

    Create .mozilla backup script

---
+++ arch/backup-dot-mozilla.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+ARCHIVE_LOCATION='/media/archive/Backup'
+ARCHIVE_NAME='dotmozilla.tar.xz'
+ARCHIVE="$ARCHIVE_LOCATION/$ARCHIVE_NAME"
+GPG_KEY='2C3CF0C7'
+
+tar -cJf "$ARCHIVE" ~/.mozilla
+gpg -er "$GPG_KEY" "$ARCHIVE"
+rm "$ARCHIVE"
+
