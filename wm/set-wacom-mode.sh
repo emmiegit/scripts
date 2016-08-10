55ea38e2d508ff37bbb8f4ec5375b8fdedf781dd
Ammon Smith <ammon.i.smith@gmail.com>
2016-08-10 14:08:44 +0900

    Add per-device wacom mode.

--- wm/set-wacom-mode.sh
+++ wm/set-wacom-mode.sh
@@ -9,16 +9,43 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
-case "$1" in
-    draw)
-        notify-send 'Setting tablet to drawing mode.'
-        xsetwacom --set 'Wacom Intuos S 2 Pen stylus' MapToOutput HEAD-0
-        ;;
-    osu)
-        notify-send 'Setting tablet to circle-clicking mode.'
-        xsetwacom --set 'Wacom Intuos S 2 Pen stylus' Area 2000 2000 8220 5500
-        ;;
-    *)
+TABLET_PART_ID='Wacom Intuos S 2 Pen stylus'
+
+case "$(hostname)" in
+	Titus)
+		# 1920 x 1200
+		case "$1" in
+			draw)
+				notify-send 'Setting tablet to drawing mode.'
+				xsetwacom --set "$TABLET_PART_ID" MapToOutput 'HEAD-0'
+				;;
+			osu)
+				notify-send 'Setting tablet to circle-clicking mode.'
+				xsetwacom --set "$TABLET_PART_ID" Area 2000 2000 8220 5500
+				;;
+			*)
+				printf >&2 'Unknown tablet mode: %s.\n' "$1"
+				exit 1
+				;;
+		esac
+		;;
+	Domitian)
+		# 1440 x 900
+		case "$1" in
+			draw)
+				notify-send 'Setting tablet to drawing mode.'
+				xsetwacom --set "$TABLET_PART_ID" MapToOutput 'LVDS-1'
+				;;
+			osu)
+				notify-send 'Setting tablet to circle-clicking mode.'
+				xsetwacom --set "$TABLET_PART_ID" Area 2000 2000 8000 5750
+				;;
+			*)
+				printf >&2 'Unknown tablet mode: %s.\n' "$1"
+				exit 1
+		esac
+		;;
+	*)
         printf >&2 'Unknown tablet mode: %s.\n' "$1"
         exit 1
         ;;
