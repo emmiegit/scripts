10633ce2db5890e61652cf84c7966a8a90a7836e
Ammon Smith <ammon.i.smith@gmail.com>
2016-09-18 22:47:03 -0700

    Add check-kernel.sh

---
+++ check-kernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+# For pacman-based distros
+KERNEL_NAME=linux
+KERNEL_SUFFIX=ARCH
+RUNNING_KERNEL="$(uname -r)"
+INSTALLED_KERNEL="$(pacman -Qi "$KERNEL_NAME" | grep '^Version' | awk '{print $3}')"
+
+if [[ "$RUNNING_KERNEL" == "$INSTALLED_KERNEL-$KERNEL_SUFFIX" ]]; then
+	printf 'Your kernel (%s) is up-to-date.\n' "$RUNNING_KERNEL"
+else
+	printf 'Your running kernel (%s) is out-of-date!\n' "$RUNNING_KERNEL"
+	printf 'The new kernel is %s.\n' "$INSTALLED_KERNEL"
+fi
+
