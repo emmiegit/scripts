02b636ad51e39e821740d8a6ad44cdfbbbb3e44b
Ammon Smith <ammon.i.smith@gmail.com>
2016-10-05 00:08:26 -0700

    Add modes to nc-play.

--- nc-play.sh
+++ nc-play.sh
@@ -17,32 +17,68 @@ play-ac() {
     mpv --no-video --speed=0.67 --audio-pitch-correction=no "$@"
 }
 
+play-random() {
+	if [[ $((RANDOM % 2)) -eq 0 ]]; then
+		FLAGS='--audio-pitch-correction=no'
+	else
+		FLAGS=
+	fi
+
+	mpv --no-video --speed="$(perl -e 'printf("%1.2f", rand() * 1.5)')" $FLAGS "$@"
+}
+
 get-song() {
 	local index="$((RANDOM % ${#SONGS[@]}))"
 	echo "${SONGS[index]}"
 }
 
 main() {
-    while true; do
+	while true; do
 		local song="$(get-song)"
-        local sel="$((RANDOM % MASK))"
 
-        case "$sel" in
-            0)
-                play "$song"
-				;;
-			1)
-				play-nc "$song"
-				;;
-			2)
-				play-ac "$song"
-				;;
-		esac
+		if [[ $MASK -eq 0 ]]; then
+			play-random "$song"
+		else
+			local sel="$((RANDOM % MASK))"
+
+			case "$sel" in
+				0)
+					play "$song"
+					;;
+				1)
+					play-nc "$song"
+					;;
+				2)
+					play-ac "$song"
+					;;
+			esac
+		fi
 	done
 }
 
-# Parse arguments and run
-SONGS=("$@")
+SONGS=()
 MASK=3
+
+# Parse arguments
+for arg in "$@"; do
+	case "$arg" in
+		-n)
+			MASK=2
+			;;
+		--nightcore-only)
+			MASK=2
+			;;
+		-s)
+			MASK=0
+			;;
+		--random-speeds)
+			MASK=0
+			;;
+		*)
+			SONGS+=("$arg")
+			;;
+	esac
+done
+
 main
 
