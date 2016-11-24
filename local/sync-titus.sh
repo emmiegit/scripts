5be6648e0128526c8d69ff7a105615d913bd2169
Ammon Smith <ammon.i.smith@gmail.com>
2016-11-23 21:25:36 -0800

    Prevent Titus from running sync script.

--- local/sync-titus.sh
+++ local/sync-titus.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -eu
 
+if [ "$(cat /etc/hostname)" == "Titus" ]; then
+	printf >&2 'Refusing to sync to self.\n'
+	exit 1
+fi
+
 if [ "$#" -eq 0 ]; then
     printf >&2 'Usage: %s path-to-sync\n' "$0"
     exit 1
