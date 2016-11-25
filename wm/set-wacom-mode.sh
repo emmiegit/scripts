124761a45b06f104390452ce0075775e701e13f6
Ammon Smith <ammon.i.smith@gmail.com>
2016-11-24 19:17:35 -0800

    Add quiet mode to wacom script.

--- wm/set-wacom-mode.sh
+++ wm/set-wacom-mode.sh
@@ -9,47 +9,74 @@ if [[ $# -eq 0 ]]; then
     exit 1
 fi
 
-TABLET_PART_ID='Wacom Intuos S 2 Pen stylus'
+mode="$1"
+tablet_part='Wacom Intuos S 2 Pen stylus'
+quiet=false
 
-case "$(hostname)" in
-	Titus)
-		# 1920 x 1200
-		case "$1" in
-			draw)
-				notify-send 'Setting tablet to drawing mode.'
-				xsetwacom --set "$TABLET_PART_ID" MapToOutput 'HEAD-0'
-				;;
-			osu)
-				notify-send 'Setting tablet to circle-clicking mode.'
-				xsetwacom --set "$TABLET_PART_ID" MapToOutput 'HEAD-0'
-				xsetwacom --set "$TABLET_PART_ID" Area 2000 2000 8220 5500
-				;;
-			*)
-				printf >&2 'Unknown tablet mode: %s.\n' "$1"
-				exit 1
-				;;
-		esac
-		;;
-	Domitian)
-		# 1440 x 900
-		case "$1" in
-			draw)
-				notify-send 'Setting tablet to drawing mode.'
-				xsetwacom --set "$TABLET_PART_ID" MapToOutput 'LVDS-1'
-				;;
-			osu)
-				notify-send 'Setting tablet to circle-clicking mode.'
-				xsetwacom --set "$TABLET_PART_ID" MapToOutput 'LVDS-1'
-				xsetwacom --set "$TABLET_PART_ID" Area 2000 2000 8000 5750
-				;;
-			*)
-				printf >&2 'Unknown tablet mode: %s.\n' "$1"
-				exit 1
-		esac
-		;;
-	*)
-        printf >&2 'Unknown tablet mode: %s.\n' "$1"
-        exit 1
-        ;;
-esac
+shift
+for arg in "$@"; do
+	case "$arg" in
+		-q)
+			quiet=true
+			;;
+		--quiet)
+			quiet=true
+			;;
+		*)
+			printf >&2 'Unknown argument: %s.\n' "$arg"
+			exit 1
+	esac
+done
+
+function notify() {
+	if ! "$quiet"; then
+		notify-send "$@"
+	fi
+}
+
+function main() {
+	case "$(hostname)" in
+		Titus)
+			# 1920 x 1200
+			case "$mode" in
+				draw)
+					notify 'Setting tablet to drawing mode.'
+					xsetwacom --set "$tablet_part" MapToOutput 'HEAD-0'
+					;;
+				osu)
+					notify 'Setting tablet to circle-clicking mode.'
+					xsetwacom --set "$tablet_part" MapToOutput 'HEAD-0'
+					xsetwacom --set "$tablet_part" Area 2000 2000 8220 5500
+					;;
+				*)
+					printf >&2 'Unknown tablet mode: %s.\n' "$mode"
+					exit 1
+					;;
+			esac
+			;;
+		Domitian)
+			# 1440 x 900
+			case "$mode" in
+				draw)
+					notify 'Setting tablet to drawing mode.'
+					xsetwacom --set "$tablet_part" MapToOutput 'LVDS-1'
+					;;
+				osu)
+					notify 'Setting tablet to circle-clicking mode.'
+					xsetwacom --set "$tablet_part" MapToOutput 'LVDS-1'
+					xsetwacom --set "$tablet_part" Area 2000 2000 8000 5750
+					;;
+				*)
+					printf >&2 'Unknown tablet mode: %s.\n' "$mode"
+					exit 1
+			esac
+			;;
+		*)
+			printf >&2 'Unknown tablet mode: %s.\n' "$mode"
+			exit 1
+			;;
+	esac
+}
+
+main
 
