eb5c0507716f48dcbd7019957a8783ba4a0e0025
Ammon Smith <ammon.i.smith@gmail.com>
2017-02-18 17:17:45 -0800

    Add sourcing.

--- apps/monster-girl-quest.sh
+++ apps/monster-girl-quest.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-set -eu
 
-export WINEARCH=win32
+export WINEARCH='win32'
 export WINEPREFIX='/media/media/Games/PlayOnLinux/wineprefix/MonsterGirlQuest'
-[[ $# -eq 1 ]] && wine "$WINEPREFIX/drive_c/Program Files/Monster Girl Quest $1/mon_que.exe"
+export WINEHOME="$WINEPREFIX"
+
+if [[ $# -eq 1 ]]; then
+	set -eu
+	LC_ALL='ja_JP.UTF-8' \
+		wine "$WINEPREFIX/drive_c/Program Files/Monster Girl Quest $1/mon_que.exe"
+fi
 
