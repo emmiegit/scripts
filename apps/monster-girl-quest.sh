3f940394fcd9f39e9af94f4a0d1b787d8ec52d02
Ammon Smith <ammon.i.smith@gmail.com>
2017-02-18 17:34:18 -0800

    Disown monster girl quest.

--- apps/monster-girl-quest.sh
+++ apps/monster-girl-quest.sh
@@ -7,6 +7,7 @@ export WINEHOME="$WINEPREFIX"
 if [[ $# -eq 1 ]]; then
 	set -eu
 	LC_ALL='ja_JP.UTF-8' \
-		wine "$WINEPREFIX/drive_c/Program Files/Monster Girl Quest $1/mon_que.exe"
+		wine "$WINEPREFIX/drive_c/Program Files/Monster Girl Quest $1/mon_que.exe" &
+	disown
 fi
 
