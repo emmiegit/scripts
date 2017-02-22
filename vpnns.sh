3944bd75d69dcd689004afec94e27f5393e8053a
Ammon Smith <ammon.i.smith@gmail.com>
2017-02-21 22:33:52 -0800

    Add subcommands.

--- vpnns.sh
+++ vpnns.sh
@@ -1,88 +1,184 @@
-#!/usr/bin/env zsh
-set -eux
+#!/bin/bash
+set -eu
 
 readonly ns_name='vpngate'
 readonly addr0='10.200.200.0'
 readonly addr1='10.200.200.1'
 readonly addr2='10.200.200.2'
+readonly dev0='vpn0'
+readonly dev1='vpn1'
 readonly subnet=24
+readonly vpn_pidfile="/run/vpnns-$ns_name.pid"
 
-if [[ $EUID != 0 ]]; then
-	echo >&2 "This script needs root privileges."
-	exec sudo "$0" "$@"
-	exit 1
-fi
+function getroot() {
+	if [[ $EUID != 0 ]]; then
+		echo >&2 "This script needs root privileges."
+		exec sudo "$0" "$@"
+		exit 1
+	fi
+}
+
+function docmd() {
+	echo "$@"
+	"$@"
+}
+
+function bgcmd() {
+	echo "$@"
+	"$@" &
+}
 
 function iface_up() {
-    ip netns add "$ns_name"
+	getroot
+	docmd ip netns add "$ns_name"
 
-    ip netns exec "$ns_name" ip addr add 127.0.0.1/8 dev lo
-    ip netns exec "$ns_name" ip link set lo up
+    docmd ip netns exec "$ns_name" ip addr add 127.0.0.1/8 dev lo
+    docmd ip netns exec "$ns_name" ip link set lo up
 
-    ip link add vpn0 type veth peer name vpn1
-    ip link set vpn0 up
-    ip link set vpn1 netns "$ns_name" up
+    docmd ip link add "$dev0" type veth peer name "$dev1"
+    docmd ip link set "$dev0" up
+    docmd ip link set "$dev1" netns "$ns_name" up
 
-    ip addr add "$addr1/$subnet" dev vpn0
-    ip netns exec "$ns_name" ip addr add "$addr2/$subnet" dev vpn1
-    ip netns exec "$ns_name" ip route add default via "$addr1" dev vpn1
+    docmd ip addr add "$addr1/$subnet" dev "$dev0"
+    docmd ip netns exec "$ns_name" ip addr add "$addr2/$subnet" dev "$dev1"
+    docmd ip netns exec "$ns_name" ip route add default via "$addr1" dev "$dev1"
 
-    iptables -A INPUT \! -i vpn0 -s "$addr0/$subnet" -j DROP
-    iptables -t nat -A POSTROUTING -s "$addr0/$subnet" -o wl+ -j MASQUERADE
+    docmd iptables -A INPUT \! -i "$dev0" -s "$addr0/$subnet" -j DROP
+    docmd iptables -t nat -A POSTROUTING -s "$addr0/$subnet" -o wl+ -j MASQUERADE
 
-    sysctl -q net.ipv4.ip_forward=1
+    docmd sysctl -q net.ipv4.ip_forward=1
 
-    mkdir -p "/etc/netns/$ns_name"
-	cp /etc/resolv.conf "/etc/netns/$ns_name/resolv.conf"
+    docmd mkdir -p "/etc/netns/$ns_name"
+	docmd cp /etc/resolv.conf "/etc/netns/$ns_name/resolv.conf"
 
-    ip netns exec "$ns_name" fping -q www.google.com
+    docmd ip netns exec "$ns_name" fping -q www.google.com
 }
 
 function iface_down() {
-    rm -rf "/etc/netns/$ns_name"
+	getroot
+	docmd rm -rf "/etc/netns/$ns_name"
 
-    sysctl -q net.ipv4.ip_forward=0
+    docmd sysctl -q net.ipv4.ip_forward=0
 
-    iptables -D INPUT \! -i vpn0 -s "$addr0/$subnet" -j DROP
-    iptables -t nat -D POSTROUTING -s "$addr0/$subnet" -o wl+ -j MASQUERADE
+    docmd iptables -D INPUT \! -i "$dev0" -s "$addr0/$subnet" -j DROP
+    docmd iptables -t nat -D POSTROUTING -s "$addr0/$subnet" -o wl+ -j MASQUERADE
 
-    ip netns delete "$ns_name"
+    docmd ip netns delete "$ns_name"
 }
 
-function run() {
-    exec sudo ip netns exec "$ns_name" "$@"
+function iface_status() {
+	if ip netns | grep "$ns_name"; then
+		echo "Interface $ns_name is UP"
+	else
+		echo "Interface $ns_name is DOWN"
+		return 1
+	fi
 }
 
-function start_vpn() {
+function ns_run() {
+	docmd exec ip netns exec "$ns_name" "$@"
+}
+
+function vpn_up() {
 	if [[ $# -eq 0 ]]; then
-		echo >&2 "Usage: $0 openvpn-file"
+		echo "Usage: $0 vpn up openvpn-file"
+		return 1
 	fi
 
-    sudo ip netns exec "$ns_name" openvpn --config "$1" &
+	getroot
+	bgcmd ip netns exec "$ns_name" openvpn --config "$1"
 
-    while ! sudo ip netns exec "$ns_name" ip a show dev tun0 up; do
+    while ! ip netns exec "$ns_name" ip a show dev tun0 up; do
         sleep .3
     done
+
+	echo "$!" > "$vpn_pidfile"
 	disown
 }
 
+function vpn_down() {
+	if [[ ! -f "$vpn_pidfile" ]]; then
+		echo "The instance isn't up"
+		return 1
+	fi
+
+	readonly local pid="$(cat "$vpn_pidfile")"
+
+	docmd kill "$pid"
+	docmd rm -f "$vpn_pidfile"
+}
+
+function vpn_status() {
+	if [[ ! -f "$vpn_pidfile" ]]; then
+		echo "The OpenVPN instance is DOWN"
+		return 1
+	fi
+
+	readonly local pid="$(cat "$vpn_pidfile")"
+
+	if kill -0 "$pid"; then
+		echo "The OpenVPN instance is UP"
+	else
+		echo "The OpenVPN instance is DEAD"
+		rm -f "$vpn_pidfile"
+		return 1
+	fi
+}
+
+function usage_and_exit() {
+	echo "Usage: $0 ns (up|down|status|exec) [arguments]"
+	echo "Usage: $0 vpn (up|down|status) [arguments]"
+	echo "Usage: $0 run [arguments]"
+	exit 1
+}
+
+if [[ $# -lt 2 ]]; then
+	usage_and_exit
+fi
+
 case "$1" in
-    up)
-        iface_up
+	ns)
+		case "$2" in
+			up)
+				iface_up
+				;;
+			down)
+				iface_down
+				;;
+			status)
+				iface_status
+				;;
+			exec)
+				shift
+				ns_run "$@"
+				;;
+			*)
+				usage_and_exit
+				;;
+		esac
 		;;
-    down)
-        iface_down
+	vpn)
+		case "$2" in
+			up)
+				vpn_up
+				;;
+			down)
+				vpn_down
+				;;
+			status)
+				vpn_status
+				;;
+			*)
+				usage_and_exit
+				;;
+		esac
 		;;
-    run)
+	run)
 		shift
-        run "$@"
+		ns_run "$@"
 		;;
-    start_vpn)
-        start_vpn "$1"
+	*)
+		usage_and_exit
 		;;
-    *)
-        echo "Syntax: $0 up|down|status|run|start_vpn|stop_vpn"
-        exit 1
-        ;;
 esac
 
