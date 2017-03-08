d69cdc0fb678ae47c890cd33e862c10c4f4f46f2
Ammon Smith <ammon.i.smith@gmail.com>
2017-03-07 16:01:45 -0800

    Add git cleanup script.

---
+++ git-gc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+if [[ $# -eq 0 ]]; then
+    readonly dirlist=('.')
+else
+    readonly dirlist=("$@")
+fi
+
+readonly origdir="$(realpath "$(dirname "$0")")"
+for dir in "${dirlist[@]}"; do
+    for repo in "$dir"/*; do
+        if [[ ! -d "$repo/.git" ]]; then
+            continue
+        fi
+
+        cd "$repo"
+		echo "Entering $repo..."
+        git fsck --full
+        git gc --aggressive
+        cd "$origdir"
+    done
+done
+
