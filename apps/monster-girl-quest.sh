a3236a6cf0060da67c7c0e44e0fce1f87c975604
Ammon Smith <ammon.i.smith@gmail.com>
2017-03-20 02:27:56 -0700

    Add MGQ script support for NG+

--- apps/monster-girl-quest.sh
+++ apps/monster-girl-quest.sh
@@ -6,8 +6,14 @@ export WINEHOME="$WINEPREFIX"
 
 if [[ $# -eq 1 ]]; then
 	set -eu
+	if [[ $1 == ng ]]; then
+		readonly local part='NG+'
+	else
+		readonly local part="Part $1"
+	fi
+
 	LC_ALL='ja_JP.UTF-8' \
-		wine "$WINEPREFIX/drive_c/Program Files/Monster Girl Quest (Part $1)/mon_que.exe" \
+		wine "$WINEPREFIX/drive_c/Program Files/Monster Girl Quest ($part)/mon_que.exe" \
 			< /dev/null \
 			> /dev/null \
 			2>&1 \
