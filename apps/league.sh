11c349e1daea003ff1118db1e624121ee7609b4a
Ammon Smith <ammon.i.smith@gmail.com>
2017-03-21 22:49:07 -0700

    Remove old app scripts.

--- apps/league.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/share/playonlinux/playonlinux --run "League of Legends" %F &
-echo "$$" | "$(dirname "$0")/league-loop"
-
--- apps/skype.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-env PULSE_LATENCY_MSEC=60 skype
-
