d9f989b6e0defec879f239c7c9bec833154abaaf
Ammon Smith <ammon.i.smith@gmail.com>
2017-03-23 11:55:32 -0700

    Fix pandoc options.

--- md2pdf.sh
+++ md2pdf.sh
@@ -4,11 +4,7 @@ set -euo pipefail
 did_something=false
 
 recursively_convert() {
-	local changed=false
-
 	for file in *; do
-		"$changed" && printf '[DIR] %s\n' "$(basename "$(pwd)")"
-
 		if [[ -d $file ]]; then
 			cd "$file"
 			recursively_convert
@@ -17,10 +13,9 @@ recursively_convert() {
 			local target="${file%.*}.pdf"
 
 			if [[ ! -f $target ]] || [[ $file -nt $target ]]; then
-				changed=true
 				did_something=true
-				printf '[MD] %s\n' "$file"
-				pandoc -f markdown_github "$file" -o "$target"
+				printf '[MD] %s\n' "$target"
+				pandoc -f markdown_github --latex-engine=xelatex -o "$target" -- "$file"
 				markdown "$file" > "$target"
 			fi
 		fi
