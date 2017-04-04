35b7116ff7678db3b6f9153ff1a55e625cf3dbfe
Ammon Smith <ammon.i.smith@gmail.com>
2017-04-04 14:24:28 -0700

    Add named credits.

--- credit-counter.py
+++ credit-counter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """
-Automatically counts credits in CYOA-type games by looking
-for instances of +(number) and -(number) and adding it all up.
+Automatically counts credits in CYOA-type games.
 """
 
 from __future__ import print_function, with_statement
@@ -9,31 +8,55 @@ import re
 import os
 import sys
 
-CREDIT_REGEX = re.compile(r'([+-][0-9]+)')
-MONEY_REGEX = re.compile(r'([+-]\$[0-9]+(?:\.[0-9]+)?k?)', re.IGNORECASE)
+COMMENT_REGEX = re.compile(r'^\s*#')
+RESET_REGEX = re.compile(r'%reset(?:\s+([A-Za-z]?))', re.IGNORECASE)
+CREDIT_REGEX = re.compile(r'([+-][0-9]+)\s*([A-Za-z]?)')
+MONEY_REGEX = re.compile(r'([+-])\$([0-9]+(?:\.[0-9]+)?k?)', re.IGNORECASE)
 
-def count_credits(text):
-    transactions = CREDIT_REGEX.findall(text)
-    credits = 0
+def get_sign(s):
+    if s == '-':
+        return -1.0
+    else:
+        return +1.0
 
-    for transaction in transactions:
-        credits += int(transaction)
+def count_credits(credits, text):
+    matches = CREDIT_REGEX.finditer(text)
 
-    return credits
+    for match in matches:
+        amount = match.group(1)
+        coin = match.group(2)
+        credits[coin] = credits.get(coin, 0) + int(amount)
 
 def count_dollars(text):
-    transactions = MONEY_REGEX.findall(text)
+    matches = MONEY_REGEX.finditer(text)
     money = 0.0
 
-    for transaction in transactions:
+    for match in matches:
         mult = 1
-        transaction = transaction.replace('$', '')
-        if transaction.endswith('k', re.IGNORECASE):
+        sign = get_sign(match.group(1))
+        amount = match.group(2)
+        if amount.endswith('k', re.IGNORECASE):
             mult = 1000
-            transaction = transaction[:-1]
-        money += float(transaction) * mult
-
+            amount = amount[:-1]
+        money += sign * float(amount) * mult
     return money
+
+def print_results(fn, credits, money):
+    credit_parts = []
+    for key in sorted(credits.keys()):
+        if key:
+            credit_parts.append("%d '%s' credits" % (credits[key], key))
+        else:
+            credit_parts.append("%d credits" % credits[key])
+    if money:
+        credit_parts.append("$%.02f per interval" % money)
+
+    if credit_parts:
+        padding = ' ' * (len(fn) + 2)
+        credit_str = ('\n' + padding).join(credit_parts)
+    else:
+        credit_str = "(none)"
+    print("%s: %s" % (fn, credit_str))
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -42,12 +65,15 @@ if __name__ == '__main__':
 
     for fn in sys.argv[1:]:
         with open(fn, 'r') as fh:
-            text = fh.read()
+            lines = fh.readlines()
 
-        credits = count_credits(text)
-        money = count_dollars(text)
-        if money:
-            print("%s: %d credits, $%.02f per interval" % (fn, credits, money))
-        else:
-            print("%s: %d credits" % (fn, credits))
+        credits = {}
+        money = 0.0
+
+        for line in lines:
+            if COMMENT_REGEX.match(line):
+                continue
+            count_credits(credits, line)
+            money += count_dollars(line)
+        print_results(fn, credits, money)
 
