4d7a0337d9f770e47c190272218433da9b779a8a
Ammon Smith <ammon.i.smith@gmail.com>
2017-04-14 13:43:55 -0700

    Add moc-shuffle script.

---
+++ moc-shuffle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eu
+
+if [[ $# -gt 0 ]] && [[ $1 -gt 0 ]]; then
+	readonly songs="$1"
+else
+	readonly songs=1
+fi
+
+i=0
+while IFS= read -r -d '' file; do
+	if [[ $i -ge $songs ]]; then
+		break;
+	fi
+
+	if ! mocp -a "$file"; then
+		continue
+	fi
+	printf 'Added "%s".\n' "$(basename "$file")"
+
+	i=$[i + 1]
+done < <(find -L "$HOME/Music" -type f -name '*.*' -print0 | shuf -z)
+
+[[ $i -eq $songs ]]
+
