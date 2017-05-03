15f2bdc72336a44801fded264e729a788e5159a2
Ammon Smith <ammon.i.smith@gmail.com>
2017-05-02 20:23:54 -0700

    Add paradox to MGQ launcher.

--- apps/monster-girl-quest.sh
+++ apps/monster-girl-quest.sh
@@ -4,21 +4,31 @@ export WINEARCH='win32'
 export WINEPREFIX='/media/media/Games/PlayOnLinux/wineprefix/MonsterGirlQuest'
 export WINEHOME="$WINEPREFIX"
 
-if [[ $# -eq 1 ]]; then
-	set -eu
-	if [[ $1 == ng ]]; then
-		readonly part='NG+'
-	else
-		readonly part="Part $1"
-	fi
-
+run() {
 	LC_ALL='ja_JP.UTF-8' \
-		wine "$WINEPREFIX/drive_c/Program Files/Monster Girl Quest ($part)/mon_que.exe" \
+		wine "$WINEPREFIX/drive_c/Program Files/$1" \
 			< /dev/null \
 			> /dev/null \
 			2> /dev/null \
 			&
 	disown
+}
+
+if [[ $# -eq 1 ]]; then
+	set -eu
+	case "$1" in
+		rpg|paradox)
+			readonly path="Monster Girl Quest Paradox/Game.exe"
+			;;
+		ng|ng+)
+			readonly path="Monster Girl Quest (NG+)/mon_que.exe"
+			;;
+		1|2|3)
+			readonly path="Monster Girl Quest (Part $1)/mon_que.exe"
+			;;
+	esac
+
+	run "$path"
 	set +eu
 else
 	echo 'Only sourcing variables. Specify which part you want to run the game.'
