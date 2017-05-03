e5cd9772ee82c3a277c72bd72aadd7d716aec06d
Ammon Smith <ammon.i.smith@gmail.com>
2017-05-02 20:24:17 -0700

    Update mirror script to use sudoedit

--- sbin/update-mirrors.sh
+++ sbin/update-mirrors.sh
@@ -3,32 +3,22 @@ set -eu
 
 echodo() {
 	echo "$@"
-	"$@"
+	sudo "$@"
 }
-
-main() {
-	if [ ! -f mirrorlist.pacnew ]; then
-		echo 'No mirrorlist.pacnew!'
-		exit 1
-	fi
-
-	echo 'Backing up old mirror list...'
-	echodo mv -f mirrorlist mirrorlist.old
-	echodo cp -f mirrorlist.pacnew mirrorlist.bak
-
-	echo 'Please uncomment any mirrors you would like to use:'
-	vim mirrorlist.bak
-
-	echo 'Ranking mirrors...'
-	rankmirrors mirrorlist.bak > mirrorlist
-}
-
-if [ $EUID != 0 ]; then
-	sudo "$0"
-	exit 0
-fi
 
 cd /etc/pacman.d
 
-main
+if [ ! -f mirrorlist.pacnew ]; then
+	echo 'No mirrorlist.pacnew!'
+	exit 1
+fi
 
+echo 'Backing up old mirror list...'
+echodo mv -f mirrorlist mirrorlist.old
+echodo cp -f mirrorlist.pacnew mirrorlist.bak
+
+echo 'Please uncomment any mirrors you would like to use:'
+sudoedit mirrorlist.bak
+
+echo 'Ranking mirrors...'
+sudo su -c 'rankmirrors mirrorlist.bak > mirrorlist'
