f0a02f46a5f906bdcd87798dfd15a11f483b936c
Ammon Smith <ammon.i.smith@gmail.com>
2017-06-25 00:29:03 -0700

    Add undo-media-hash

---
+++ archv/undo-media-hash.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import re
+import os
+import sys
+
+HASH_LIST_REGEX = re.compile(r'"(.*)" -> "(.*)"\s*')
+
+def rename(old, new):
+    if not os.path.exists(old):
+        print("'{}' doesn't exist".format(old))
+    elif os.path.exists(new):
+        print("'{}' already exists, not overwriting".format(new))
+    elif old == new:
+        print("Source and destination are the same ({})".format(old))
+    else:
+        print("'{}' -> '{}'".format(old, new))
+        os.rename(old, new)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        fn = 'hash-renamed-files.txt'
+    else:
+        fn = sys.argv[1]
+
+    if not os.path.isfile(fn):
+        print("Can't find '{}'".format(fn))
+        exit(1)
+
+    with open(fn, 'r') as fh:
+        for line in fh.readlines():
+            match = HASH_LIST_REGEX.match(line)
+            if match is None:
+                print("Non-matching line: '{}'".format(line.rstrip()))
+            rename(match.group(2), match.group(1))
+
