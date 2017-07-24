5aa09ea421608ac4f17b2a412b053a4e9c937677
Ammon Smith <ammon.i.smith@gmail.com>
2017-07-24 09:11:03 -0700

    Update pickle-dump.

--- pickle-dump.py
+++ pickle-dump.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python
 
 from pprint import pprint
+import argparse
+import os
 import pickle
 import sys
 
 if __name__ == '__main__':
-    ret = 0
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-I', '--import', dest='modules', action='append',
+            help="List of modules to import before dumping the pickle.")
+    argparser.add_argument('filenames', nargs='+',
+            help="List of files to inspect.")
+    args = argparser.parse_args()
 
-    for fn in sys.argv[1:]:
-        print("Contents of '%s':" % fn)
+    for module in args.modules:
+        exec(f"import {module}")
+
+    success = True
+    for fn in args.filenames:
+        print(f"Contents of '{fn}':")
         try:
             with open(fn, 'rb') as fh:
                 obj = pickle.load(fh)
             pprint(obj)
         except Exception as ex:
-            print("Failure: %r" % ex)
-            ret = 1
+            print(f"Failure: {ex!r}")
+            success = False
 
-        if fn is not sys.argv[-1]:
-            print()
-
-    exit(ret)
+    exit(int(success))
 
