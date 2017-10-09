97dc2327d7c481c16d19f5ec3aaf0d3a06544d20
Ammon Smith <ammon.i.smith@gmail.com>
2017-10-09 00:16:03 -0700

    Update backup backup script.

--- sbin/backup-backup.sh
+++ sbin/backup-backup.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-sudo rsync -vrtpAXlHogS --progress --safe-links --delete-after /media/archive /media/ExternalDisk
+set -x
+
+exec sudo \
+	ionice -c 3 \
+		nice rsync \
+		-vrtpAXlHogS \
+		--progress \
+		--safe-links \
+		--delete-after \
+		"$@" -- /media/archive/* /media/ExternalDisk
+
