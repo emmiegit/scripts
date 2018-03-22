8dbe11bbb9bdc6595bd92a054acc9005352a9451
Ammon Smith <ammon.i.smith@gmail.com>
2018-03-21 21:13:49 -0700

    Fix $parallel

--- update-repos.sh
+++ update-repos.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 source "$(dirname "$0")/dat/repos"
 
 background=true
+parallel=false
 fsck=false
 gc=false
 
@@ -53,13 +54,19 @@ update() {
 main() {
 	for repo in "${repos[@]}" "$@"; do
 		echo "$repo"
-		update "$repo"
+
+		if "$parallel"; then
+			update "$repo" &
+		else
+			update "$repo"
+		fi
 	done
 	wait
 }
 
 if "$background"; then
 	echo 'Running in background.'
+	parallel=true
 	main </dev/null >/dev/null 2>&1 &
 	disown
 else
