9d395e9e958e68a1bffa4d51be9bbf59a20ac0e5
Ammon Smith <ammon.i.smith@gmail.com>
2018-03-30 00:23:53 -0700

    Remove dir creation from wm/startup.sh

--- wm/startup.sh
+++ wm/startup.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
 set -eu
 
-dir_make() {
-	for dir in "$@"; do
-		mkdir -m700 -p "/tmp/$USER/$dir"
-	done
-}
-
-mkdir -m750 "/tmp/$USER"
-dir_make aur
-dir_make cache
-dir_make vim_undo
-dir_make private
-
 mkdir -m777 -p "/tmp/shared"
-
