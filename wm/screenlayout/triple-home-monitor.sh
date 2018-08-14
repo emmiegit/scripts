aa6c1bdd419e0b1dd8bde3ef95676995343a39c2
Ammon Smith <ammon.i.smith@gmail.com>
2018-08-14 01:49:29 -0400

    Format triple-home-monitor.sh

--- wm/screenlayout/triple-home-monitor.sh
+++ wm/screenlayout/triple-home-monitor.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-xrandr --output VGA-0 --mode 1280x1024 --pos 3840x0 --rotate normal --output DVI-D-0 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DVI-D-1 --mode 1920x1200 --pos 0x0 --rotate normal --output HDMI-0 --off
+exec xrandr \
+	--output VGA-0 \
+		--mode 1280x1024 \
+		--pos 3840x0 \
+		--rotate normal \
+	--output DVI-D-0 \
+		--primary \
+		--mode 1920x1200 \
+		--pos 1920x0 \
+		--rotate normal \
+	--output DVI-D-1 \
+		--mode 1920x1200 \
+		--pos 0x0 \
+		--rotate normal \
+	--output HDMI-0 \
+		--off
