fbe221676567c428c8d84951a3e094eb544f37d8
Ammon Smith <ammon.i.smith@gmail.com>
2018-08-14 01:49:53 -0400

    Fix startup sequence and idemptotence.

--- wm/startup.sh
+++ wm/startup.sh
@@ -11,6 +11,4 @@ mkdir -m777 -p "/tmp/shared"
 dir_make cache
 dir_make vim_undo
 dir_make private
-ln -s "/tmp/$USER/aur" "$HOME/.cache/yay"
-
-/usr/local/scripts/wm/screenlayout/run.sh
+ln -sf "/tmp/$USER/aur" "$HOME/.cache/yay"
