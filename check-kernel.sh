831dfd3ce8c8551cc17129b2ef89d8b7a3924774
Ammon Smith <ammon.i.smith@gmail.com>
2018-08-14 19:59:49 -0400

    Update check-kernel.sh

--- check-kernel.sh
+++ check-kernel.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
+
+verbose=false
 
 # For pacman-based distros
-KERNEL_NAME=linux
-KERNEL_SUFFIX=ARCH
-RUNNING_KERNEL="$(uname -r)"
-INSTALLED_KERNEL="$(pacman -Qi "$KERNEL_NAME" | grep '^Version' | awk '{print $3}')"
+kernel_name=linux
+kernel_suffix=ARCH
+running_kernel="$(uname -r)"
+installed_kernel="$(pacman -Qi "$kernel_name" | grep '^Version' | awk '{print $3}')"
+mod_installed_kernel="$(sed -e 's/\.arch/-arch/' <<< "$installed_kernel")"
+full_installed_kernel="$mod_installed_kernel-$kernel_suffix"
 
 for arg in "$@"; do
 	case "$arg" in
-		-q)
-			printf() { :; }
+		-v|--verbose)
+			verbose=true
 			;;
-		--quiet)
+		-q|--quiet)
 			printf() { :; }
 			;;
 	esac
 done
 
-if [[ "$RUNNING_KERNEL" == "$INSTALLED_KERNEL-$KERNEL_SUFFIX" ]]; then
-	printf 'Your kernel (%s) is up-to-date.\n' "$RUNNING_KERNEL"
-else
-	printf 'Your running kernel (%s) is out-of-date!\n' "$RUNNING_KERNEL"
-	printf 'The new kernel is %s.\n' "$INSTALLED_KERNEL"
-	exit 1
+if "$verbose"; then
+	printf '$kernel_name:           "%s"\n' "$kernel_name"
+	printf '$kernel_suffix:         "%s"\n' "$kernel_suffix"
+	printf '$running_kernel:        "%s"\n' "$running_kernel"
+	printf '$installed_kernel:      "%s"\n' "$installed_kernel"
+	printf '$mod_installed_kernel:  "%s"\n' "$mod_installed_kernel"
+	printf '$full_installed_kernel: "%s"\n' "$full_installed_kernel"
+	echo
 fi
 
+if [[ "$running_kernel" == "$full_installed_kernel" ]]; then
+	printf 'Your kernel (%s) is up-to-date.\n' "$running_kernel"
+else
+	printf 'Your running kernel (%s) is out-of-date!\n' "$running_kernel"
+	printf 'The new kernel is %s.\n' "$full_installed_kernel"
+	exit 1
+fi
