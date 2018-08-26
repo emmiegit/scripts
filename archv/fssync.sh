31842146a0747fc9624a48a774ae564170299b25
Ammon Smith <ammon.i.smith@gmail.com>
2018-08-26 15:50:23 -0400

    Fix shellcheck errors.

--- archv/fssync.sh
+++ archv/fssync.sh
@@ -2,26 +2,27 @@
 
 # fssync - Two-way rsync
 fssync() {
-	case $# in
-	3)
-		rsync $1 "$2" "$3" &&
-		rsync $1 "$3" "$2" ;;
-	2)
-		rsync -avhl "$1" "$2" &&
-		rsync -avhl "$2" "$1" ;;
-	*)
-		echo "Usage: fssync [options] first-directory second-directory"
-		return 1
+	case "$#" in
+		3)
+			rsync "$1" "$2" "$3" &&
+			rsync "$1" "$3" "$2" ;;
+		2)
+			rsync -avhl "$1" "$2" &&
+			rsync -avhl "$2" "$1" ;;
+		*)
+			echo "Usage: fssync [options] first-directory second-directory"
+			return 1
 	esac
 	
-	printf "Finished synchronizing $1 and $2.\n\n"
+	echo "Finished synchronizing $1 and $2."
+	echo
 }
 
 fssync_nolink() {
-	case $# in
+	case "$#" in
 	3)
-		rsync $1 "$2" "$3" &&
-		rsync $1 "$3" "$2" ;;
+		rsync "$1" "$2" "$3" &&
+		rsync "$1" "$3" "$2" ;;
 	2)
 		rsync -avh --no-links "$1" "$2" &&
 		rsync -avh --no-links "$2" "$1" ;;
@@ -30,6 +31,6 @@ fssync_nolink() {
 		return 1
 	esac
 	
-	printf "Finished synchronizing $1 and $2.\n\n"
+	echo "Finished synchronizing $1 and $2."
+	echo
 }
-
