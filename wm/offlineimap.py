3bef8ca42494a89fdeeda8ee54dfe6dba301e420
Ammon Smith <ammon.i.smith@gmail.com>
2018-12-20 19:13:55 -0500

    Remove offlineimap.py

--- wm/offlineimap.py
+++
@@ -1,9 +0,0 @@
-#!!/usr/bin/env python2
-__all__ = ["get_pass"]
-
-from subprocess import check_output
-import os.path
-
-def get_pass():
-    return check_output(["gpg", "-dq", os.path.expanduser("~/.mutt/passwd.gpg")]).strip("\n")
-
