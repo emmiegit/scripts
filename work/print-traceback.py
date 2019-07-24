59b920d3c5d699c180be4dafec86f50a0c636028
Ammon Smith <ammon.i.smith@gmail.com>
2019-07-24 12:30:08 -0400

    Improve stacktrace print for traceback.

--- work/print-traceback.py
+++ work/print-traceback.py
@@ -4,19 +4,31 @@ from pprint import pprint
 import json
 import sys
 
-if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        path = sys.argv[1].split('.')
-    else:
-        path = ['error', 'stack']
-
-    obj = json.load(sys.stdin)
-
+def get(obj, path):
     try:
         for part in path:
             obj = obj[part]
+        return obj
     except KeyError:
-        pass
+        return None
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        paths = [sys.argv[1].split('.')]
+    else:
+        paths = [
+            ['meta', 'error', 'stack'],
+            ['error', 'stack'],
+            ['traceback'],
+        ]
+
+    obj = json.load(sys.stdin)
+
+    for path in paths:
+        subobj = get(obj, path)
+        if subobj is not None:
+            obj = subobj
+            break
 
     if isinstance(obj, str):
         print(obj)
