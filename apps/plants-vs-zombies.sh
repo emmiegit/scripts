46ad3e47ac81ac04a9a216a0be49feff3a6afa0b
Ammon Smith <ammon.i.smith@gmail.com>
2019-09-08 13:39:52 -0400

    Move plants-vs-zombies.

--- apps/plants-vs-zombies.sh
+++ apps/plants-vs-zombies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export WINEARCH='win32'
-export WINEPREFIX='/media/media/Games/PlayOnLinux/wineprefix/PlantsVsZombies'
+export WINEPREFIX='/media/media/games/wine/plants-vs-zombies'
 export WINEHOME="$WINEPREFIX"
 
 cd "$WINEPREFIX/drive_c/games/Pogo Games/Plants Vs Zombies Game of the Year Edition"
