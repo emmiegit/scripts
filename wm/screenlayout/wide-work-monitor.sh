31bd9d1f8ffb3fbc6c6dc6c53cc395f5f11d6cdf
Ammon Smith <ammon.i.smith@gmail.com>
2019-12-31 13:15:14 -0500

    Adjust wide monitor configuration.

--- wm/screenlayout/wide-work-monitor.sh
+++ wm/screenlayout/wide-work-monitor.sh
@@ -4,9 +4,8 @@ set -eu
 /usr/local/scripts/wm/new-wallpaper.sh
 
 exec xrandr \
-	--output VIRTUAL1 \
-		--off \
 	--output eDP1 \
+		--primary \
 		--mode 1920x1080 \
 		--pos 0x536 \
 		--rotate normal \
@@ -14,13 +13,15 @@ exec xrandr \
 		--off \
 	--output DP2 \
 		--off
-	--output HDMI1 \
-		--off \
 	--output DP1-1 \
+		--off \
+	--output DP1-2 \
 		--mode 2560x1080 \
 		--pos 1920x0 \
 		--rotate normal \
-	--output DP1-2 \
-		--off \
 	--output DP1-3 \
+		--off \
+	--output HDMI1 \
+		--off \
+	--output VIRTUAL1 \
 		--off
