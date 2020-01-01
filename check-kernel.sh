50804bef4a76389260c40ddb5ab0900d4eaa210c
Ammon Smith <ammon.i.smith@gmail.com>
2020-01-01 16:13:57 -0500

    Update check-kernel.sh script

--- check-kernel.sh
+++ check-kernel.sh
@@ -5,11 +5,9 @@ verbose=false
 
 # For pacman-based distros
 kernel_name=linux
-kernel_suffix=ARCH
 running_kernel="$(uname -r)"
 installed_kernel="$(pacman -Qi "$kernel_name" | grep '^Version' | awk '{print $3}')"
 mod_installed_kernel="$(sed -e 's/\.arch/-arch/' <<< "$installed_kernel")"
-full_installed_kernel="$mod_installed_kernel-$kernel_suffix"
 
 for arg in "$@"; do
 	case "$arg" in
@@ -23,19 +21,17 @@ for arg in "$@"; do
 done
 
 if "$verbose"; then
-	printf '$kernel_name:           "%s"\n' "$kernel_name"
-	printf '$kernel_suffix:         "%s"\n' "$kernel_suffix"
-	printf '$running_kernel:        "%s"\n' "$running_kernel"
-	printf '$installed_kernel:      "%s"\n' "$installed_kernel"
-	printf '$mod_installed_kernel:  "%s"\n' "$mod_installed_kernel"
-	printf '$full_installed_kernel: "%s"\n' "$full_installed_kernel"
+	printf '$kernel_name:          "%s"\n' "$kernel_name"
+	printf '$running_kernel:       "%s"\n' "$running_kernel"
+	printf '$installed_kernel:     "%s"\n' "$installed_kernel"
+	printf '$mod_installed_kernel: "%s"\n' "$mod_installed_kernel"
 	echo
 fi
 
-if [[ "$running_kernel" == "$full_installed_kernel" ]]; then
+if [[ "$running_kernel" == "$mod_installed_kernel" ]]; then
 	printf 'Your kernel (%s) is up-to-date.\n' "$running_kernel"
 else
 	printf 'Your running kernel (%s) is out-of-date!\n' "$running_kernel"
-	printf 'The new kernel is %s.\n' "$full_installed_kernel"
+	printf 'The new kernel is %s.\n' "$mod_installed_kernel"
 	exit 1
 fi
