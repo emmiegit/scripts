32f474059c41e7166b7bddc466d3eb2765e01bb0
Ammon Smith <ammon.i.smith@gmail.com>
2020-01-18 17:35:17 -0500

    Add directory specifiers to tarsnap script.

--- sbin/tarsnap-backup.sh
+++ sbin/tarsnap-backup.sh
@@ -2,19 +2,33 @@
 set -eu
 
 archive="$(date +%B-%d-%Y | tr '[:upper:]' '[:lower:]')"
-locations=(
-	"$HOME"
-	"/etc"
-	"/var"
-	"/media/media"
-	"/media/archive"
-)
+locations=()
 
+case "$1" in
+	root)
+		locations+=(
+			"$HOME"
+			"/etc"
+			"/var"
+		)
+		;;
+	archive)
+		locations+=('/media/archive')
+		;;
+	media)
+		locations+=('/media/media')
+		;;
+	*)
+		echo "Unknown location specifier: $1"
+		exit 1
+esac
+
+set -x
 exec \
 	ionice -c 3 \
 	nice -n 10 \
 		tarsnap -c \
-		-f "$archive" \
+		-f "$archive-$1" \
 		-v \
 		--print-stats \
 		"${locations[@]}"
