63d034db37ce64cd91eb572dbe4a378db4faf846
Ammon Smith <ammon.i.smith@gmail.com>
2020-02-22 16:55:46 -0500

    Add new minecraft backup to archive.

--- archv/backup-minecraft.sh
+++ archv/backup-minecraft.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -euo pipefail
+set -eux
+set -o pipefail
 
-cd "$HOME/documents/games/minecraft-servers/$1"
+cd "$HOME/documents/games/minecraft-servers/$1/backups"
 echo "Backing up Minecraft server '$1'..."
 
-readonly backup_dir="_backups/$(date +'%Y-%m-%d')"
+readonly backup_dir="$(date +'%Y-%m-%d')"
 mkdir -p "$backup_dir"
 cp -an world world_nether world_the_end "$backup_dir"
+7z a archive.7z "$backup_dir"
+rm -rf "$backup_dir"
