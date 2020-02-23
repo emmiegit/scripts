6b15b931b113fa673fb5d37360a5dbf5f52f34e4
Ammon Smith <ammon.i.smith@gmail.com>
2020-02-22 21:21:22 -0500

    Revert "Prepare for remote-hosted minecraft."
    This reverts commit 2b9261fca8ed37aa7473c76d925dcacd63682ecc.

--- archv/backup-minecraft.sh
+++ archv/backup-minecraft.sh
@@ -2,19 +2,11 @@
 set -eux
 set -o pipefail
 
-readonly backup_dir="$(mktemp -d)/$(date +'%Y-%m-%d')"
-
-mkdir "$backup_dir"
-cd "$HOME/documents/games/minecraft-servers/$1"
+cd "$HOME/documents/games/minecraft-servers/$1/backups"
 echo "Backing up Minecraft server '$1'..."
 
-rsync \
-	-vrtpAXl \
-	-zz \
-	--safe-links \
-	"$@" \
-	"Adam:/usr/games/minecraft/$1/"{world,world_nether,world_the_end} \
-	"$backup_dir"
-
+readonly backup_dir="$(date +'%Y-%m-%d')"
+mkdir -p "$backup_dir"
+cp -an world world_nether world_the_end "$backup_dir"
 7z a archive.7z "$backup_dir"
 rm -rf "$backup_dir"
