67550f8b71e5d94597941ea54042b95ac42a3775
Ammon Smith <ammon.i.smith@gmail.com>
2020-03-29 19:49:40 -0400

    Add full scan mode for mpc update.

--- wm/media/update.sh
+++ wm/media/update.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
+# Settings
+full=false
+
+while getopts ':f' opt; do
+	case "$opt" in
+		f)
+			full=true
+			;;
+		:)
+			printf >&2 "Option %s requires an argument\n" "$OPTARG"
+			exit 1
+			;;
+		\?)
+			printf >&2 "Invalid argument: '-%s'\n" "$OPTARG"
+			exit 1
+			;;
+	esac
+done
+shift "$((OPTIND - 1))"
+
+# Run mpd command
 source "${0%/*}/mpc.sh"
-mpc update
+
+if "$full"; then
+	mpc rescan
+else
+	mpc update --wait
+fi
