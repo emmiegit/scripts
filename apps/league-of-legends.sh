85b0e97879abc05c1d48e3c95f81b6f9a6ee0393
Ammon Smith <ammon.i.smith@gmail.com>
2020-04-10 00:28:51 -0400

    Update league script.

--- apps/league-of-legends.sh
+++ apps/league-of-legends.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+export PATH=/opt/wine-lol/bin
 export WINEPREFIX=/media/media/games/wine/league-of-legends
 export WINEHOME="$WINEPREFIX"
 export WINEARCH=win32
@@ -8,4 +9,22 @@ export MESA_GL_VERSION_OVERRIDE=4.5COMPAT
 export MESA_GLTHREAD=TRUE
 export VBLANK_MODE=0
 
-exec wine 'C:\Riot Games\League of Legends\LeagueClient.exe' >/tmp/league-output-$EUID.log 2>&1
+output="/tmp/league-output-$EUID.log"
+
+while getopts :dk opt; do
+	case "$opt" in
+		d)
+			output="/dev/stdout"
+			;;
+		k)
+			wineserver -k
+			exit 0
+			;;
+		\?)
+			echo "Invalid arg: -$OPTARG" >&2
+			exit 1
+			;;
+	esac
+done
+
+exec /opt/wine-lol/bin/wine 'C:\Riot Games\League of Legends\LeagueClient.exe' >"$output" 2>&1
