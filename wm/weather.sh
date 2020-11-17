a6dcb2d3ab77564b90268291e104a13cb75d1422
Ammon Smith <ammon.i.smith@gmail.com>
2020-11-17 14:50:11 -0500

    Update weather.sh location

--- wm/weather.sh
+++ wm/weather.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-location=92521
+location=10021
 
 curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?locCode\=${location}               \
 	| perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }' \
 	| sed 's/F/°F/g'
 echo
-
