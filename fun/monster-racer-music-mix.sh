f80d7c11a57550c9e2b8e367f68ecf60b96f0ee8
Ammon Smith <ammon.i.smith@gmail.com>
2021-02-13 03:13:14 -0500

    Add ability to play bosses, with names.

--- fun/monster-racer-music-mix.sh
+++ fun/monster-racer-music-mix.sh
@@ -9,16 +9,20 @@ if [[ $# -eq 0 ]]; then
 	echo "Artist can be one of:"
 	echo " + 4"
 	echo " + chp"
-	echo " - kath"
+	echo " - 7"
+	echo " - aur"
 	echo " - csx"
 	echo " - csxxeo"
-	echo " - sn"
-	echo " - SG"
-	echo " - XEO"
+	echo " - kath"
+	echo " - onea"
+	echo " - onea_quiet"
+	echo " - pt"
 	echo " - quiet"
-	echo " - 7"
+	echo " - sn"
+	echo " - xeo"
 	echo " - FJ"
 	echo " - OCR"
+	echo " - SG"
 	echo
 	echo "Where '+' means it's a full soundtrack,"
 	echo "and '-' means it's incomplete."
@@ -30,7 +34,17 @@ artist="$1"
 shift
 arguments=("$@")
 
-play() {
+# Playback functions
+play_lobby() {
+	file="lobby_${artist}.ogg"
+
+	if [[ -f $file ]]; then
+		echo "Playing: Lobby"
+		mpv "${arguments[@]}" "$file"
+	fi
+}
+
+play_floor() {
 	zone="$1"
 	floor="$2"
 	variant="$3"
@@ -52,13 +66,59 @@ play() {
 	fi
 }
 
+play_boss() {
+	boss="$1"
+
+	case "$boss" in
+		1)
+			boss_name='King Conga'
+			;;
+		2)
+			boss_name='Death Metal'
+			;;
+		3)
+			boss_name='Deep Blues'
+			;;
+		4)
+			boss_name='Coral Riff'
+			;;
+		5)
+			boss_name='Dead Ringer'
+			;;
+		6)
+			boss_name='Necrodancer'
+			;;
+		8)
+			boss_name='Golden Lute'
+			;;
+		9)
+			boss_name='Fortissimole'
+			;;
+		11)
+			boss_name='The Conductor'
+			;;
+	esac
+
+	file="boss_${boss}_${artist}.ogg"
+
+	if [[ -f $file ]]; then
+		echo "Playing: ${boss_name}"
+		mpv "${arguments[@]}" "$file"
+	fi
+}
+
+# Main
+play_lobby
+
 for zone in {1..5}; do
 	for floor in {1..3}; do
 		if [[ $zone == 3 ]]; then
-			play "$zone" "$floor" c
-			play "$zone" "$floor" h
+			play_floor "$zone" "$floor" c
+			play_floor "$zone" "$floor" h
 		else
-			play "$zone" "$floor" ''
+			play_floor "$zone" "$floor" ''
 		fi
 	done
+
+	play_boss "$zone"
 done
