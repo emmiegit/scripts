dacb3173adb3cba7cd0f0bb8f80ac4be37c9974d
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-09 11:22:09 -0400

    Remove duplicate tmp file creation.

--- wm/startup.sh
+++ wm/startup.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 set -eu
 
-dir_make() {
-	for dir in "$@"; do
-		mkdir -m700 -p "/tmp/$USER/$dir"
-	done
-}
-
-mkdir -m777 -p "/tmp/shared"
-dir_make aur
-dir_make cache
-dir_make vim_undo
-dir_make private
 ln -sf "/tmp/$USER/aur" "$HOME/.cache/yay"
 
 /usr/local/scripts/wm/screenlayout/run.sh
