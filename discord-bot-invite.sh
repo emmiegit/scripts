9882c9986fa3077dd3e1445823cf8150e421e051
Ammon Smith <ammon.i.smith@gmail.com>
2021-11-10 21:12:34 -0500

    Fix percent substitution.

--- discord-bot-invite.sh
+++ discord-bot-invite.sh
@@ -2,4 +2,4 @@
 set -eu
 
 [[ $# -eq 0 ]] && exit 1
-printf 'https://discordapp.com/oauth2/authorize?scope=bot%20applicatinos.commands&client_id=%s&scope=bot&permissions=0\n' "$1"
+printf 'https://discordapp.com/oauth2/authorize?scope=bot%%20applicatinos.commands&client_id=%s&scope=bot&permissions=0\n' "$1"
