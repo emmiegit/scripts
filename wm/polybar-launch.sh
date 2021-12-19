75660f30713f79b0923dd78ff53599c6d5112649
Ammon Smith <ammon.i.smith@gmail.com>
2021-12-18 20:54:36 -0500

    Add error for empty invocation.

--- wm/polybar-launch.sh
+++ wm/polybar-launch.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eu
 
+# Check arguments
+if [[ $# -eq 0 ]]; then
+	echo >&2 "Usage: $0 <bar-name>..."
+	exit 1
+fi
+
 # Stop old instances
 if compgen -G /tmp/polybar_mqueue.*; then
 	polybar-msg cmd quit
