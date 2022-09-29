0c174c57b19151e43e49cc8090c3610194c48d18
Ammon Smith <ammon.i.smith@gmail.com>
2022-09-28 21:07:02 -0400

    Update screenlayout configuration.

--- wm/screenlayout/triple-home-monitor.sh
+++ wm/screenlayout/triple-home-monitor.sh
@@ -7,18 +7,26 @@ set -eu
 ) &
 
 exec xrandr \
-	--output VGA-0 \
+	--output DVI-D-0 \
 		--mode 1280x1024 \
 		--pos 3840x0 \
 		--rotate normal \
-	--output DVI-D-0 \
+	--output DP-0 \
+		--off \
+	--output DP-1 \
+		--off \
+	--output DP-2 \
+		--off \
+	--output DP-3 \
+		--mode 1920x1200 \
+		--pos 0x0 \
+		--rotate normal \
+	--output DP-4 \
+		--off \
+	--output DP-5 \
 		--primary \
 		--mode 1920x1200 \
 		--pos 1920x0 \
 		--rotate normal \
-	--output DVI-D-1 \
-		--mode 1920x1200 \
-		--pos 0x0 \
-		--rotate normal \
 	--output HDMI-0 \
-		--off
+		--same-as DP-5
