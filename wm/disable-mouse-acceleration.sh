b43b74db06f79fb6cae45731f6f8a4ee26ef9c07
Ammon Smith <ammon.i.smith@gmail.com>
2022-09-30 14:44:38 -0400

    Adjust value.

--- wm/disable-mouse-acceleration.sh
+++ wm/disable-mouse-acceleration.sh
@@ -2,7 +2,7 @@
 set -ux
 
 # Device-specific settings
-xinput --set-prop '2.4G Mouse' 'libinput Accel Speed' 0.25
+xinput --set-prop '2.4G Mouse' 'libinput Accel Speed' -0.25
 xinput --set-prop 'USB Optical Mouse' 'libinput Accel Speed' -0.25
 
 # Disable acceleration
