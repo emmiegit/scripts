f27fc3bf104e81062def80179ee4da37788f4ac7
Ammon Smith <ammon.i.smith@gmail.com>
2022-10-18 01:27:41 -0400

    Add wineserver -k option.

--- apps/plants-vs-zombies.sh
+++ apps/plants-vs-zombies.sh
@@ -6,7 +6,9 @@ export WINEHOME="$WINEPREFIX"
 
 cd "$WINEPREFIX/drive_c/games/Pogo Games/Plants Vs Zombies Game of the Year Edition"
 
-if [[ -z $VERBOSE ]]; then
+if [[ -z $KILL ]]; then
+	wineserver -k
+elif [[ -z $VERBOSE ]]; then
 	wine 'PlantsVsZombies.ifn' \
 		< /dev/null \
 		> /dev/null \
