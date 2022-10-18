10eec74549665eb6cdff0f0425fbb5f1d39fa542
Ammon Smith <ammon.i.smith@gmail.com>
2022-10-18 01:25:22 -0400

    Add "verbose" mode to script.

--- apps/plants-vs-zombies.sh
+++ apps/plants-vs-zombies.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 export WINEARCH='win32'
 export WINEPREFIX='/media/media/games/wine/plants-vs-zombies'
 export WINEHOME="$WINEPREFIX"
 
 cd "$WINEPREFIX/drive_c/games/Pogo Games/Plants Vs Zombies Game of the Year Edition"
-wine 'PlantsVsZombies.ifn' \
-	< /dev/null \
-	> /dev/null \
-	2> /dev/null \
-	&
-disown
+
+if [[ -z $VERBOSE ]]; then
+	wine 'PlantsVsZombies.ifn' \
+		< /dev/null \
+		> /dev/null \
+		2> /dev/null \
+		&
+	disown
+else
+	wine 'PlantsVsZombies.ifn' &
+fi
