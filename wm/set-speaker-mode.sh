1f9026b97cdf411d928bf9c9c111a86ba0a0d707
Ammon Smith <ammon.i.smith@gmail.com>
2023-01-06 22:04:48 -0500

    Create set-speaker-mode.sh

---
+++ wm/set-speaker-mode.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -eu
+
+if [[ $# -eq 0 ]]; then
+	printf >&2 'Usage: %s (headphones | tv)\n' "$(basename "$0")"
+	exit 1
+fi
+
+mode="$1"
+quiet=false
+headphones_sink='alsa_output.pci-0000_12_00.3.analog-stereo'
+television_sink='alsa_output.pci-0000_0b_00.1.hdmi-stereo'
+
+shift
+for arg in "$@"; do
+	case "$arg" in
+		-q)
+			quiet=true
+			;;
+		--quiet)
+			quiet=true
+			;;
+		*)
+			printf >&2 'Unknown argument: %s.\n' "$arg"
+			exit 1
+	esac
+done
+
+function notify() {
+	if [[ $# -gt 0 ]] && ! "$quiet"; then
+		notify-send "$@"
+	fi
+}
+
+function set_sink() {
+	pactl set-default-sink "$1"
+}
+
+case "$mode" in
+	headphones|hp)
+		notify 'Setting computer to headphone mode.'
+		set_sink "$headphones_sink"
+		;;
+	television|tv)
+		notify 'Setting computer to television mode.'
+		set_sink "$television_sink"
+		;;
+	*)
+		printf >&2 'Unknown speaker setting: %s.\n' "$mode"
+		exit 1
+esac
