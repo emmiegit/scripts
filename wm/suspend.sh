d31c73f30f10c839d86f6fcd6cadefbe59abf87b
Emmie Maeda <emmie.maeda@gmail.com>
2023-03-17 19:21:14 -0400

    Restart ibus on suspend finish.

--- wm/suspend.sh
+++ wm/suspend.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eu
 
+# Pre-suspend preparation
 /usr/local/scripts/wm/i3-lock.py
 xset dpms force suspend
+
+# Suspension
 systemctl suspend
+
+# Post-suspend recovery
+systemctl restart ibus.service
