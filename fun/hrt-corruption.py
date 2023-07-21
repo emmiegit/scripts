fe3e3a0f4f14159e2e79746b7566cdf4d08ec167
Emmie Maeda <emmie.maeda@gmail.com>
2023-07-21 01:30:04 -0400

    Add clipboard copy support.

--- fun/hrt-corruption.py
+++ fun/hrt-corruption.py
@@ -4,11 +4,18 @@
 
 from datetime import datetime
 
-BAR_LEVELS =  "░▒▓█"
+try:
+    import pyperclip
+    CLIPBOARD = True
+except ImportError:
+    CLIPBOARD = False
+
+BAR_LEVELS = "░▒▓█"
 BAR_SIZE = 12
 
 START_DAY = datetime(2023, 6, 9)
 FULL_CORRUPTION = 2 * 365
+
 
 def make_bar(percent):
     result = [None] * BAR_SIZE
@@ -25,8 +32,16 @@ def make_bar(percent):
 
     return "".join(result)
 
+
+def copy_to_clipboard(text):
+    if CLIPBOARD:
+        pyperclip.copy(text)
+        print("Copied to clipboard!")
+
+
 if __name__ == "__main__":
     days_since = (datetime.now() - START_DAY).days
     percent = days_since / FULL_CORRUPTION  # between 0 and 1
     bar = make_bar(percent)
     print(f"Corruption progress: {bar} {percent * 100:.1f}%")
+    copy_to_clipboard(bar)
