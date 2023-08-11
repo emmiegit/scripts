79b88c4a610b0d00a242b401a56dfc03f9e7a5b0
Emmie Maeda <emmie.maeda@gmail.com>
2023-08-11 00:54:16 -0400

    Add line.sh app launch script.

--- apps/line.sh
+++ apps/line.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-export WINEARCH='win32'
 export WINEPREFIX='/media/media/games/wine/line'
 export WINEHOME="$WINEPREFIX"
 
 cd "$WINEHOME/drive_c/users/$USER/AppData/Local/LINE/bin"
+
+case $1 in
+	exit|quit|kill)
+		wineserver -k
+		exit 0
+		;;
+esac
+
 wine LineLauncher.exe \
 	< /dev/null \
 	> /dev/null \
-	2> /dev/null
+	2>&1
