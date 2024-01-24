0e0e01b057c76ca5e4cb5c65a8d849875727ffe0
Emmie Maeda <emmie.maeda@gmail.com>
2024-01-23 20:04:33 -0500

    Re-enable suspend.

--- wm/suspend.sh
+++ wm/suspend.sh
@@ -5,9 +5,8 @@ set -eu
 /usr/local/scripts/wm/i3-lock.py
 xset dpms force suspend
 
-# TODO suspend currently broken due to kernel update
-## Suspension
-#systemctl suspend
-#
-## Post-suspend recovery
-#systemctl restart ibus.service
+# Suspension
+systemctl suspend
+
+# Post-suspend recovery
+systemctl --user restart ibus.service
