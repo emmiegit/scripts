751aa980f5fdab968a6b80d325de10d8c61a8392
Emmie Maeda <emmie.maeda@gmail.com>
2024-06-01 09:34:19 -0400

    Remove script.

--- apps/confirm-shred.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if zenity --question \
-		  --title "Permanently delete files." \
-		  --text "Are you sure you want to shred these files?\nThey will be permanently deleted from your computer."; then
-	shred -u "$@"
-fi
-
