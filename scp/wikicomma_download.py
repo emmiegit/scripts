6cd1897b5ecbd676a5c2cc62c4cd086d281a9676
Emmie Maeda <emmie.maeda@gmail.com>
2025-02-01 21:55:18 -0500

    Start full wikicomma download script.

---
+++ scp/wikicomma_download.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import argparse
+import re
+import os
+import shutil
+import subprocess
+import sys
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+TORRENT_DIRECTORY = "/tmp/emmie/wikicomma"
+DOWNLOADS_DIRECTORY = "/media/archive/temporary/wikicomma"
+UPLOAD_SSH_SERVER = "rsync.net"
+UPLOAD_SSH_PATH = "./wikicomma"
+
+TORRENT_URL_REGEX = re.compile(r"/snapshots/[^\.]+\.torrent/[^\.]+\.torrent")
+WIKICOMMA_URL_REGEX = re.compile(
+    r"https://wikidot.dbotthepony.ru/snapshots/([^\.]+)\.torrent/?"
+)
+
+
+def download_torrent_files(url):
+    r = requests.get(args.url)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    torrent_files = []
+
+    print("Downloading torrent files")
+    for link in soup.select("td.fb-n a"):
+        href = link["href"]
+        if TORRENT_URL_REGEX.fullmatch(href) is None:
+            # skip invalid links, like the ".." parent link
+            continue
+
+        torrent_url = urljoin(args.url, href)
+        filename = os.path.basename(href)
+
+        r = requests.get(torrent_url, stream=True)
+        torrent_file = os.path.join(TORRENT_DIRECTORY, filename)
+        with open(torrent_file, "wb") as file:
+            print(f"+ {filename}")
+            for chunk in r.iter_content(chunk_size=512):
+                file.write(chunk)
+        torrent_files.append(torrent_file)
+    return torrent_files
+
+
+def download_torrent(torrent_file):
+    print(f"Downloading Wikicomma torrent data ({torrent_file})")
+    subprocess.check_call(
+        ["aria2c", torrent_file],
+        cwd=DOWNLOADS_DIRECTORY,
+    )
+
+    torrent_name = os.path.basename(torrent_file)
+    path = os.path.join(DOWNLOADS_DIRECTORY, torrent_name)
+    assert os.path.isdir(path), f"Download for {torrent_file} did not write to {path}"
+    return path
+
+
+def upload_data(source, destination):
+    print(f"Uploading Wikicomma data to remote server ({destination})")
+    subprocess.check_call(["rsync", "-vahHP", "-zz", source, destination])
+
+
+def cleanup_data(directory):
+    print(f"Finished upload, deleting ({directory})")
+    shutil.rmtree(download_path)
+
+
+def transfer_torrents(torrent_date, torrent_files):
+    destination_path = os.path.join(UPLOAD_SSH_PATH, torrent_date, directory)
+    destination = f"{UPLOAD_SSH_SERVER}:{destination_path}"
+
+    for torrent_file in torrent_files:
+        download_path = download_torrent(torrent_file)
+        upload_data(download_path, destination)
+        cleanup_data(download_path)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("url")
+    args = argparser.parse_args()
+
+    os.makedirs(TORRENT_DIRECTORY, exist_ok=True)
+    os.makedirs(DOWNLOADS_DIRECTORY, exist_ok=True)
+
+    match = WIKICOMMA_URL_REGEX.fullmatch(args.url)
+    if match is None:
+        print("Warning: URL does not match known Wikicomma torrent path")
+        sys.exit(1)
+
+    torrent_date = match[1]
+    torrent_files = download_torrent_files(args.url)
+    transfer_torrents(torrent_date, torrent_files)
