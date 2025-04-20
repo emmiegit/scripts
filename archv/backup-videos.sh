f0a179c1c9b4a54cb510832897bedaab6bc4fb4c
Emmie Maeda <emmie.maeda@gmail.com>
2025-04-20 18:51:02 -0400

    Add backup script for video list.

---
+++ archv/backup-videos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+exec \
+	ionice -c 3 \
+		nice rsync \
+			-vahH \
+			--progress \
+			--preallocate \
+			--delete-after \
+			--exclude=temporary/ \
+			"$@" -- /media/media/videos/{anime,films,television} rsync.net:./videos/
+
