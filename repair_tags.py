e4eb29ed016134b39f0212715f77066f93bf31f2
Emmie Maeda <emmie.maeda@gmail.com>
2025-05-02 01:50:41 -0400

    Start repair_tags.py script.

---
+++ repair_tags.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import subprocess
+import sys
+
+ID3_INFO_REGEX = re.compile(r"=== ([^ ]+) \(.+\): (.+)")
+
+
+def is_opus(path):
+    _, ext = os.path.splitext(path)
+    return ext.casefold() == ".opus"
+
+
+def valid_tags(path):
+    result = subprocess.run(
+        ["opustags", path],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    return result.returncode == 0
+
+
+def get_id3_tags(path):
+    metadata = {}
+    output = subprocess.check_output(["id3info", path]).decode("utf-8")
+    for line in output.split("\n"):
+        match = ID3_INFO_REGEX.match(line)
+        if match is None:
+            continue
+
+        tag, value = match[1], match[2]
+        # See https://docs.mp3tag.de/mapping-table/
+        match tag:
+            case "TPE1":
+                metadata["--artist"] = value
+            case "TALB":
+                metadata["--album"] = value
+            case "TSOA":
+                metadata["--album-artist"] = value
+            case "TIT2":
+                metadata["--title"] = value
+            case "TRCK":
+                metadata["--track"] = value
+            case "COMM":
+                metadata["--comment"] = value
+            case "TYER":
+                metadata["--year"] = value
+            case "TCON":
+                metadata["--genre"] = value
+            case _:
+                raise ValueError(f"Unknown ID3 tag: {tag}")
+
+    return metadata
+
+
+def repair_file(path):
+    print(f"Repairing file {path}")
+
+    # Retrieve existing tags
+    metadata = get_id3_tags(path)
+
+    # Clear ID3 tags
+    subprocess.check_call(["id3convert", "-s", path])
+
+    # Re-apply tags using autotag in non-automatic mode (lol)
+    arguments = ["tagit", "-x"]
+    for key, value in metadata.items():
+        arguments.extend((key, value))
+    arguments.append(path)
+    subprocess.check_call(arguments)
+
+
+def repair_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        print(f"Checking {root}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        directory = sys.argv[1]
+        print(f"Repairing directory {directory}")
+    else:
+        directory = "."
+        print("Repairing current directory")
+
+    repair_directory(directory)
