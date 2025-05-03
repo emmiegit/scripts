c3888d06466b15f5adede0a52ffba1c47c8b6008
Emmie Maeda <emmie.maeda@gmail.com>
2025-05-02 22:01:43 -0400

    Start RipDownloader class.

--- fun/siivagunner-download.py
+++ fun/siivagunner-download.py
@@ -7,86 +7,119 @@ import subprocess
 import sys
 from tempfile import TemporaryDirectory
 
-YOUTUBE_DL_PROGRAM = "yt-dlp"
-AUTOTAGGER_PROGRAM = os.path.expanduser("~/git/scripts/autotag.py")
-
 UNMAP = {
     "：": ":",
-    "＂": "\"",
+    "＂": '"',
     # TODO |, \, ?
 }
 
 
 def unmap_windows_chars(path):
     new_path = path
-
     for part, sub in UNMAP.items():
         new_path = new_path.replace(part, sub)
-
     return new_path if new_path != path else None
 
 
-def download_audio(directory, url):
-    output = os.path.join(directory, "%(title)s.%(ext)s")
-    command = [YOUTUBE_DL_PROGRAM, url, "--no-playlist", "-x", "-o", output]
-    print(command)
-    subprocess.check_output(command)
+class RipDownloader:
+    def __init__(self, args):
+        self.temp_dir = None
+        self.dest_dir = args.destination
+        self.youtube_dl_program = args.youtube_downloader
+        self.autotagger_script = args.autotagger_script if args.autotag else None
 
+    def __enter__(self):
+        self.temp_dir = TemporaryDirectory(prefix="rip")
+        return self
 
-def process_files(temp_dir, dest="."):
-    paths_to_tag = []
-    for path in os.listdir(temp_dir):
-        if os.path.isfile(path):
-            continue
+    def __exit__(self, exc_type, exc_value, tb):
+        self.temp_dir.cleanup()
 
-        # Replace Windows-safe replacement characters, if present
-        new_path = unmap_windows_chars(path)
-        if new_path is not None:
-            old_sourcefile = os.path.join(temp_dir, path)
-            new_sourcefile = os.path.join(temp_dir, new_path)
-            os.rename(old_sourcefile, new_sourcefile)
-            print(f"{old_sourcefile} -> {new_sourcefile}")
-            path = new_path
-
-        sourcefile = os.path.join(temp_dir, path)
-        filename, ext = os.path.splitext(path)
-        parts = filename.split(" - ")
-        match len(parts):
-            case 2:
-                # Move to album subdirectory
-                title, album = parts
-                album_dir = os.path.join(dest, album)
-                os.makedirs(album_dir, exist_ok=True)
-                print(f"{filename} -> {album_dir}")
-
-                destfile = os.path.join(album_dir, title + ext)
-                shutil.move(sourcefile, destfile)
-                paths_to_tag.append(destfile)
-            case 1:
-                # Single title song, like fusion collabs
-                print(f"{filename} -> {dest}")
-
-                destfile = shutil.move(sourcefile, dest)
-                paths_to_tag.append(destfile)
-            case _:
-                print(f"Cannot interpret filename: {filename}", file=sys.stderr)
-                shutil.move(sourcefile, dest)
-
-
-    if paths_to_tag and AUTOTAGGER_PROGRAM is not None:
-        command = [AUTOTAGGER_PROGRAM] + paths_to_tag
+    def download_audio(self, url):
+        output = os.path.join(self.temp_dir, "%(title)s.%(ext)s")
+        command = [self.youtube_dl_program, url, "--no-playlist", "-x", "-o", output]
         print(command)
         subprocess.check_output(command)
 
+    def process_files(self):
+        paths_to_tag = []
+        for path in os.listdir(self.temp_dir):
+            if os.path.isfile(path):
+                continue
+
+            # Replace Windows-safe replacement characters, if present
+            new_path = unmap_windows_chars(path)
+            if new_path is not None:
+                old_sourcefile = os.path.join(self.temp_dir, path)
+                new_sourcefile = os.path.join(self.temp_dir, new_path)
+                os.rename(old_sourcefile, new_sourcefile)
+                print(f"{old_sourcefile} -> {new_sourcefile}")
+                path = new_path
+
+            sourcefile = os.path.join(self.temp_dir, path)
+            filename, ext = os.path.splitext(path)
+            parts = filename.split(" - ")
+            match len(parts):
+                case 2:
+                    # Move to album subdirectory
+                    title, album = parts
+                    album_dir = os.path.join(self.dest_dir, album)
+                    os.makedirs(album_dir, exist_ok=True)
+                    print(f"{filename} -> {album_dir}")
+
+                    destfile = os.path.join(album_dir, title + ext)
+                    shutil.move(sourcefile, destfile)
+                    paths_to_tag.append(destfile)
+                case 1:
+                    # Single title song, like fusion collabs
+                    print(f"{filename} -> {self.dest_dir}")
+
+                    destfile = shutil.move(sourcefile, self.dest_dir)
+                    paths_to_tag.append(destfile)
+                case _:
+                    print(f"Cannot interpret filename: {filename}", file=sys.stderr)
+                    shutil.move(sourcefile, self.dest_dir)
+
+        if paths_to_tag and self.autotagger_script is not None:
+            command = [self.autotagger_script] + paths_to_tag
+            print(command)
+            subprocess.check_output(command)
+
+
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser(description="Download assistant for SiIva-style rips")
+    argparser = argparse.ArgumentParser(
+        name="siivagunner-download",
+        description="Download assistant for SiIva-style rips",
+    )
+    argparser.add_argument(
+        "-y",
+        "--youtube-downloader",
+        dest="youtube_downloader",
+        default="yt-dlp",
+        help="The YouTube downloader program to use",
+    )
+    argparser.add_argument(
+        "-T",
+        "--autotagger-script",
+        dest="autotagger_script",
+        default=os.path.expanduser("~/git/scripts/autotag.py"),
+        help="The music autotagger script to use",
+    )
     argparser.add_argument(
         "-t",
         "--no-tag",
         "--no-autotag",
-        output="autotag",
+        dest="autotag",
         action="store_false",
-        help="Disable the autotagger after downloading and placing the rip",
+        help="Disable the autotagger after downloading and placing the rip(s)",
+    )
+    argparser.add_argument(
+        "-d",
+        "--dest",
+        "--destination",
+        dest="destination",
+        default=".",
+        help="Specify an alternate download directory for the rip(s)",
     )
     argparser.add_argument(
         "url",
@@ -95,8 +128,7 @@ if __name__ == "__main__":
     )
     args = argparser.parse_args()
 
-    with TemporaryDirectory(prefix="rip") as temp_dir:
+    with RipDownloader(args) as downloader:
         for url in args.url:
-            download_audio(temp_dir, url)
-
-        process_files(temp_dir)
+            downloader.download_audio(url)
+        downloader.process_files()
