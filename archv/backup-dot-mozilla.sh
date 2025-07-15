3d7e542991c524926562cc2ec3bfb1912f60f6ed
Emmie Maeda <emmie.maeda@gmail.com>
2025-07-15 02:12:36 -0400

    Add open tab list backup.

--- archv/backup-dot-mozilla.sh
+++ archv/backup-dot-mozilla.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 archive_location='/media/archive/backup'
 archive_name='dotmozilla.tar.xz'
 archive="$archive_location/$archive_name"
+tabs_name='firefox_tabs.txt'
+tabs="$archive_location/$tabs_name"
+firefox_profile='0s56fc3h.dev-edition-default'
 gpg_key='2C3CF0C7'
 tar_program='/usr/bin/tar'
 temp_dir="$(mktemp -d /tmp/dot-mozilla-XXXXXXXXX)"
@@ -26,3 +29,7 @@ cp -a ~/.mozilla "$temp_dir"
 
 # Encrypt using the given GPG key
 gpg --yes -er "$gpg_key" "$archive"
+
+# Export open browser tabs
+dejsonlz4 "$HOME/.mozilla/firefox/$firefox_profile/sessionstore.jsonlz4" "$temp_dir/sessionstore.json"
+jq -r '.windows[].tabs[].entries[].url' "$temp_dir/sessionstore.json" > "$tabs"
