90de822f490fbfb72fd939fd36cac72f5de54ed6
Emmie Maeda <emmie.maeda@gmail.com>
2025-09-02 02:51:24 -0400

    Begin video exclusion list in sync.

--- archv/backup-videos-rsync.sh
+++ archv/backup-videos-rsync.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -eu
 
+# Note that the exclude section has videos I have deleted from my local computer,
+# but retain on remote in case I wish to rewatch it some time later.
+
 exec \
 	ionice -c 3 \
 		nice rsync \
 			-vahH \
 			--progress \
-			--exclude=temporary/ \
+			--exclude=films/Conclave \
+			--exclude=films/Flightplan \
 			"$@" -- /media/media/videos/{anime,films,television} rsync.net:./videos/
-
