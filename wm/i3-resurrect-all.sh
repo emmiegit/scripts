9938a52a7f04b80eeb58e1b2dc6dcd0e47549d4d
Emmie Maeda <emmie.maeda@gmail.com>
2025-11-09 13:51:36 -0500

    Remove fixed directory.
    Since it now gets it from a fixed configuration file? idk

--- wm/i3-resurrect-all.sh
+++ wm/i3-resurrect-all.sh
@@ -2,5 +2,5 @@
 set -eu
 
 for workspace in {1..4}; do
-	i3-resurrect restore --directory "$HOME/.config/i3/i3-resurrect" --workspace "$workspace"
+	i3-resurrect restore --workspace "$workspace"
 done
