87021e970f8898377a6f2362d67ebbeae94a2193
Emmie Maeda <emmie.maeda@gmail.com>
2025-11-13 01:56:27 -0500

    Add to startup too?

--- wm/startup.sh
+++ wm/startup.sh
@@ -4,5 +4,7 @@ set -eu
 mkdir -p "/tmp/$USER/"{aur,cache,private,vim_undo}
 ln -sf "/tmp/$USER/aur" "$HOME/.cache/yay"
 
+/usr/local/scripts/wm/vi-keyswap.sh
+/usr/local/scripts/wm/reset-repeat-rate.sh
 /usr/local/scripts/wm/screenlayout/run.sh
 /usr/local/scripts/wm/set-speaker-mode.sh headphones
