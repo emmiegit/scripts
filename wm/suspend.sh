66b3d19954890687c68d8385f4a212823318eef5
Emmie Maeda <emmie.maeda@gmail.com>
2025-11-17 00:25:07 -0500

    Remove unnecessary keyboard resets.
    The ibus service restart is what's causing it, so doing stuff before is
    meaningless.

--- wm/suspend.sh
+++ wm/suspend.sh
@@ -9,6 +9,4 @@ xset dpms force suspend
 systemctl suspend
 
 # Post-suspend recovery
-/usr/local/scripts/wm/vi-keyswap.sh
-/usr/local/scripts/wm/reset-repeat-rate.sh
 systemctl --user restart ibus.service
