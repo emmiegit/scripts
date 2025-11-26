795894150d2b70e267252eba5985d9e357b2e4e8
Emmie Maeda <emmie.maeda@gmail.com>
2025-11-26 12:18:16 -0500

    Set workspaces to restore dynamically.

--- wm/i3-resurrect-all.sh
+++ wm/i3-resurrect-all.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -eu
 
-for workspace in {1..4}; do
+case "$(cat /etc/hostname)" in
+	Titus)
+		workspaces=({1..4})
+		;;
+	Augustus)
+		workspaces=({1..5} 7)
+		;;
+	*)
+		notify-msg "Unknown hostname $(cat /etc/hostname), cannot determine workspaces to restore"
+		exit 1
+esac
+
+for workspace in "${workspaces[@]}"; do
 	i3-resurrect restore --workspace "$workspace"
 done
