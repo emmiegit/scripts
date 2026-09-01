3fec1dec61bf29bc2f3bf1ed779b7df1e039d9c7
Emmie Smith <git@emmie.tech>
2026-09-01 00:57:21 -0400

    Try new setup for update-nvidia.sh

--- sbin/update-nvidia.sh
+++ sbin/update-nvidia.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
+set -eux
 
 # Updates all nvidia driver packages
 #
 # Since they don't appear together in sorted output
 # but need to be built together
 
-# TODO figure out how to best implement this
-#      since saying "just install these packages"
-#      in a row doesn't actually work...
-#
-#      perhaps cloning and doing makepkg on the relevant
-#      base packages then installing the *.pkg.tar files?
-
-echo yay -Sy \
+# List of packages
+readonly packages=(
 	nvidia-580xx-dkms
 	nvidia-580xx-settings
 	nvidia-580xx-utils
@@ -20,3 +15,24 @@ echo yay -Sy \
 	lib32-opencl-nvidia-580xx
 	opencl-nvidia-580xx
 	libxnvctrl-580xx
+)
+
+# Download packages
+(
+	cd "$AURDEST"
+	for pkg in "${packages[@]}"; do
+		git clone "https://aur.archlinux.org/$pkg.git"
+	done
+)
+
+# Build each package
+(
+	cd "$AURDEST"
+	for pkg in "${packages[@]}"; do
+		[[ ! -d $pkg/.git ]] && continue
+		makepkg
+	done
+)
+
+# Install all the packages
+sudo pacman -U "$AURDEST/**/*.pkg.tar
